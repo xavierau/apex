04a7042e0024e505306c80a14c342ef65deeabdf
xavierau <xavier.au@gmail.com>
2014-12-04 20:47:58 +0800

    modified navbar css

--- app/views/front/partials/head.blade.php
+++ app/views/front/partials/head.blade.php
@@ -3,7 +3,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>@yield('title','CMS')</title>
     <meta name="description" content="@yield('meta_discription')">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
     @include('front.partials.blocks.css')
     @yield('style')
 </head>
