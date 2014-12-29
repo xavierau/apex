600d925a6a13f637b7cd29cbd61da9aa7a09971c
xavierau <xavier.au@gmail.com>
2014-12-30 06:55:48 +0800

    RC2 with a dd for header and then removed

--- app/views/front/partials/header.blade.php
+++ app/views/front/partials/header.blade.php
@@ -2,7 +2,6 @@
     <img class="logo" src="{{asset('assets/imgs/logo-bg.jpg')}}"/>
     <div class="lang-links pull-right">
         <ol class="breadcrumb pull-right">
-            {{dd(Cache::get('languages'))}}
             @foreach(Cache::get('languages') as $language)
                 <li><a href="{{route('setLang',$language['iso_code'])}}">{{$language['language']}}</a></li>
             @endforeach
