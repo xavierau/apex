c5839bece0253fe81cef81e2d0cdd01586a9fef4
xavierau <xavier.au@gmail.com>
2014-12-30 14:33:12 +0800

    RC3 update eviornment variable

---
+++ app/config/forge/database.php
@@ -0,0 +1,20 @@
+<?php 
+
+return array(
+
+		'connections' => array(
+
+			'mysql' => array(
+				'driver'    => 'mysql',
+				'host'      => '128.199.230.226',
+				'database'  => 'forge',
+				'username'  => 'forge',
+				'password'  => '5Gm3iOqzrgEjrtlEpGKb',
+				'charset'   => 'utf8',
+				'collation' => 'utf8_unicode_ci',
+				'prefix'    => '',
+			),
+
+		),
+
+);
