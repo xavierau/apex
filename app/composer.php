ba5ce08057b3fd3f593a5b3d6e16b81fa43f1078
Xavier Au <xavier.au@anacreation.com>
2016-05-16 17:56:23 +0800

    left side-bar intro text

--- app/composer.php
+++ app/composer.php
@@ -2,9 +2,11 @@
 
 // Backend View Composer
 View::composer('front.partials.blocks.MainMenu','Acme\Composers\front\MainMenuComposer');
+View::composer('front.partials.blocks.sidebar-intro','Acme\Composers\front\AboutComposer');
 View::composer('front.pages.immigration','Acme\Composers\front\ImmigrationComposer');
 View::composer('front.pages.about','Acme\Composers\front\AboutComposer');
 View::composer('front.pages.index-temp','Acme\Composers\front\IndexComposer');
+View::composer('front.pages.index','Acme\Composers\front\IndexComposer');
 View::composer('front.pages.index','Acme\Composers\front\IndexComposer');
 
 View::composer('system.partials.blocks.top-navbar','Acme\Composers\system\SystemNavigationComposer');
